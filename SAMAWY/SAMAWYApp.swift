6834129fd386852b9b19417d4286e96995ea8ad1
deveLujain <lujainashahri@gmail.com>
2024-10-02 09:27:26 +0300

    Initial Commit

---
+++ SAMAWY/SAMAWYApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAMAWYApp.swift
+//  SAMAWY
+//
+//  Created by Lujain sh on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SAMAWYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
